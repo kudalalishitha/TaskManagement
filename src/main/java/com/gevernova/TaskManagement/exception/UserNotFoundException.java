ce70c20de2907c7fe8be0d2eb827ea9ed7fefc9c
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-11 15:04:04 +0530

    [lishitha]:UserNotFoundException

---
+++ src/main/java/com/gevernova/TaskManagement/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gevernova.TaskManagement.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
