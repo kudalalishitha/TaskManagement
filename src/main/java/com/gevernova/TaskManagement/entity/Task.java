a094669c1cacbcf35f37067d4732730a1882e5cf
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-11 14:52:33 +0530

    [lishitha]:Task entity with relationships

---
+++ src/main/java/com/gevernova/TaskManagement/entity/Task.java
@@ -0,0 +1,37 @@
+package com.gevernova.TaskManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "priority_id")
+    private Priority priority;
+}
