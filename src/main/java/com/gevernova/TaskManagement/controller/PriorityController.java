8841f6575c0cba226cfba2e85124ef62f71bd245
Lishitha Kudala <kudalalishitha@gmail.com>
2026-02-11 15:03:21 +0530

    [lishitha]:Add REST APIs for Priority management

---
+++ src/main/java/com/gevernova/TaskManagement/controller/PriorityController.java
@@ -0,0 +1,43 @@
+package com.gevernova.TaskManagement.controller;
+
+import com.gevernova.TaskManagement.entity.Priority;
+import com.gevernova.TaskManagement.service.PriorityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/priorities")
+@RequiredArgsConstructor
+public class PriorityController {
+
+    private final PriorityService priorityService;
+
+    @PostMapping
+    public ResponseEntity<Priority> createPriority(@RequestBody Priority priority) {
+        return new ResponseEntity<>(priorityService.createPriority(priority), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Priority>> getAllPriorities() {
+        return ResponseEntity.ok(priorityService.getAllPriorities());
+    }
+
+    @GetMapping("/{priorityId}")
+    public ResponseEntity<Priority> getPriorityById(@PathVariable Long priorityId) {
+        return ResponseEntity.ok(priorityService.getPriorityById(priorityId));
+    }
+
+    @PutMapping("/{priorityId}")
+    public ResponseEntity<Priority> updatePriority(@PathVariable Long priorityId, @RequestBody Priority priority) {
+        return ResponseEntity.ok(priorityService.updatePriority(priorityId, priority));
+    }
+
+    @DeleteMapping("/{priorityId}")
+    public ResponseEntity<String> deletePriority(@PathVariable Long priorityId) {
+        return ResponseEntity.ok(priorityService.deletePriority(priorityId));
+    }
+}
