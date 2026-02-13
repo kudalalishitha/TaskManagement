a4b04b02189c80d7b2163b49666cb629a50f92f0
kudalalishitha <165412597+kudalalishitha@users.noreply.github.com>
2026-02-13 09:59:20 +0530

    [lishitha]:Implement CRUD operations in TaskController

--- src/main/java/com/gevernova/TaskManagement/controller/TaskController.java
+++ src/main/java/com/gevernova/TaskManagement/controller/TaskController.java
@@ -16,34 +16,41 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TaskController {
 
+     // Service layer object
     private final TaskService taskService;
 
+    // Create a new Task
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody TaskRequest request) {
         return new ResponseEntity<>(taskService.createTask(request), HttpStatus.CREATED);
     }
 
+    // Get all Tasks
     @GetMapping
     public ResponseEntity<List<Task>> getAllTasks() {
         return ResponseEntity.ok(taskService.getAllTasks());
     }
 
+     // Get a Task by ID
     @GetMapping("/{taskId}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long taskId) {
         return ResponseEntity.ok(taskService.getTaskById(taskId));
     }
 
+    // Update a Task by ID
     @PutMapping("/{taskId}")
     public ResponseEntity<Task> updateTask(@PathVariable Long taskId, @RequestBody TaskRequest request) {
         return ResponseEntity.ok(taskService.updateTask(taskId, request));
     }
 
+     // Update only the status of a Task
     @PutMapping("/{taskId}/status")
     public ResponseEntity<Task> updateTaskStatus(@PathVariable Long taskId,
                                                  @RequestBody TaskStatusUpdateRequest request) {
         return ResponseEntity.ok(taskService.updateTaskStatus(taskId, request));
     }
 
+     // Delete a Task by ID
     @DeleteMapping("/{taskId}")
     public ResponseEntity<String> deleteTask(@PathVariable Long taskId) {
         return ResponseEntity.ok(taskService.deleteTask(taskId));
